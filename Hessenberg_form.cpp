2c58ea2c92f6c20e61c4f87b419fbb7aafa3f4d8
Leonid Burtsev <l.bustaev@s02210338@gse.cs.msu.ru>
2023-11-07 21:56:22 +0300

    fix cicles

--- Hessenberg_form.cpp
+++ Hessenberg_form.cpp
@@ -67,9 +67,9 @@ void
 l_matrix_multiplicate(double* matrix, double* x, int& size_of_matrix, int size_of_vector, double& norm)
 {
     int dif = size_of_matrix - size_of_vector;
-
+    double sum;
     for(int i = dif - 1; i < size_of_matrix; i++) {
-        double sum = 0;
+        sum = 0;
         for(int j = dif; j < size_of_matrix; j++) {
             sum += (matrix[i *  size_of_matrix + j] * x[j - dif]);
         }
@@ -89,8 +89,12 @@ r_matrix_multiplicate(double* matrix, double* x, int& size_of_matrix, int size_o
 
     for(int j = dif; j < size_of_matrix; j++) {
         for(int i = dif - 1; i < size_of_matrix; i++) {
-            vector_sum[i - dif + 1] += (matrix[j * size_of_matrix + i] * x[j - dif]) / norm;
+            vector_sum[i - dif + 1] += (matrix[j * size_of_matrix + i] * x[j - dif]);
         }
+    }
+
+    for(int i = 0; i < size_of_matrix - dif + 1; i++) {
+        vector_sum[i] /= norm;
     }
 
     for(int j = dif; j < size_of_matrix; j++) {
