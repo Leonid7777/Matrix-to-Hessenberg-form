21dbed564bdcf4e204298ca0af374bf56aacda44
Leonid7777 <90553178+Leonid7777@users.noreply.github.com>
2023-11-07 20:52:32 +0300

    fix r_multiplicate

--- Hessenberg_form.cpp
+++ Hessenberg_form.cpp
@@ -53,28 +53,31 @@ matrix_make(double* matrix, int& size_of_matrix)
 void
 make_vector_x(int& i, int& size_of_matrix, double* x, double* matrix, double& norm)
 {
-    double sum = 0;
+    norm = 0;
     for(int j = i + 1; j < size_of_matrix; j++) {
         x[j - i - 1] = matrix[j * size_of_matrix + i];
-        sum += matrix[j * size_of_matrix + i] * matrix[j * size_of_matrix + i];
+        norm += matrix[j * size_of_matrix + i] * matrix[j * size_of_matrix + i];
     }
-    x[0] -= sqrt(sum);
-    norm = (sum - matrix[(i + 1) * size_of_matrix + i] * matrix[(i + 1) * size_of_matrix + i] + x[0] * x[0]) / 2;
+    x[0] -= sqrt(norm);
+    norm -= matrix[(i + 1) * size_of_matrix + i] * matrix[(i + 1) * size_of_matrix + i] - x[0] * x[0];
+    norm /= 2;
 }
 
 void
 l_matrix_multiplicate(double* matrix, double* x, int& size_of_matrix, int size_of_vector, double& norm)
 {
     int dif = size_of_matrix - size_of_vector;
-    
-    for(int i = dif - 1; i < size_of_matrix; i++) {
-        double sum = 0;
-        for(int j = dif; j < size_of_matrix; j++) {
-            sum += (matrix[j * size_of_matrix + i] * x[j - dif]);
+    double* vector_sum = new double[size_of_matrix - dif + 1];
+
+    for(int j = dif; j < size_of_matrix; j++) {
+        for(int i = dif - 1; i < size_of_matrix; i++) {
+            vector_sum[i - dif + 1] += (matrix[j * size_of_matrix + i] * x[j - dif]) / norm;
         }
-        sum /= norm;
-        for(int j = dif; j < size_of_matrix; j++) {
-            matrix[j * size_of_matrix + i] -= (x[j - dif] * sum);
+    }
+
+    for(int j = dif; j < size_of_matrix; j++) {
+        for(int i = dif - 1; i < size_of_matrix; i++) {
+            matrix[j * size_of_matrix + i] -= (x[j - dif] * vector_sum[i - dif + 1]);
         }
     }
 }
@@ -99,30 +102,30 @@ r_matrix_multiplicate(double* matrix, double* x, int& size_of_matrix, int size_o
 int
 main(void)
 {
-    auto start = std::chrono::high_resolution_clock::now();
-    int size_of_matrix = 1024;
+    int size_of_matrix = 5;
     // size_of_matrix_create(size_of_matrix);
     double* matrix = new double[size_of_matrix * size_of_matrix];
     matrix_make(matrix, size_of_matrix);
+    double norm = 0;
+    double* x = new double[size_of_matrix - 1];
+
+    auto start = std::chrono::high_resolution_clock::now();
     for(int i = 0; i < size_of_matrix - 2; i++) {
-        double* x = new double[size_of_matrix - i - 1];
-        double norm;
         make_vector_x(i, size_of_matrix, x, matrix, norm);
         l_matrix_multiplicate(matrix, x, size_of_matrix, size_of_matrix - i - 1, norm);
         r_matrix_multiplicate(matrix, x, size_of_matrix, size_of_matrix - i - 1, norm);
     }
-    
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
     std::cout << std::endl;
     
-    // for(int i = 0; i < size_of_matrix; i++) {
-    //     for(int j = 0; j < size_of_matrix; j++) {
-    //         std::cout << matrix[i *  size_of_matrix + j] << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
+    for(int i = 0; i < size_of_matrix; i++) {
+        for(int j = 0; j < size_of_matrix; j++) {
+            std::cout << matrix[i *  size_of_matrix + j] << " ";
+        }
+        std::cout << std::endl;
+    }
     
     bool all_is_good = true;
     
