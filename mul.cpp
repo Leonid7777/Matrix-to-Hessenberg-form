4f6c1e476c6946f74bf275d1b8ccef3f181bba30
Leonid Burtsev <l.bustaev@s02210338@gse.cs.msu.ru>
2023-12-18 21:07:37 +0300

    Better code

--- mul.cpp
+++ mul.cpp
@@ -1,4 +1,21 @@
 void
+l_matrix_multiplicate(double* matrix, double* x, int size_of_matrix, int size_of_vector, double norm)
+{
+    int dif = size_of_matrix - size_of_vector;
+    double sum;
+    for(int i = dif - 1; i < size_of_matrix; i++) {
+        sum = 0;
+        for(int j = dif; j < size_of_matrix; j++) {
+            sum += (matrix[i *  size_of_matrix + j] * x[j - dif]);
+        }
+        sum /= norm;
+        for(int j = dif; j < size_of_matrix; j++) {
+            matrix[i *  size_of_matrix + j] -= (x[j - dif] * sum);
+        }
+    }
+}
+
+void
 r_matrix_multiplicate(double* matrix, double* x, int size_of_matrix, int size_of_vector, double norm)
 {
     int dif = size_of_matrix - size_of_vector;
@@ -21,3 +38,4 @@ r_matrix_multiplicate(double* matrix, double* x, int size_of_matrix, int size_of
         }
     }
 }
+
---
+++ mul.h
@@ -0,0 +1,2 @@
+void r_matrix_multiplicate(double* matrix, double* x, int size_of_matrix, int size_of_vector, double norm);
+void l_matrix_multiplicate(double* matrix, double* x, int size_of_matrix, int size_of_vector, double norm);
