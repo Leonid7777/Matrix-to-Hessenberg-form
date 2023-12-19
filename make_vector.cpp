5992a7e28da4948d6be4cdf0730b0b49cceff5db
Leonid7777 <90553178+Leonid7777@users.noreply.github.com>
2023-12-19 13:20:44 +0300

    Update make_vector.cpp

--- make_vector.cpp
+++ make_vector.cpp
@@ -9,7 +9,7 @@ make_vector_x(int& i, int size_of_matrix, double* x, double* matrix, double& nor
         x[j - i - 1] = matrix[pas + j];
         norm += matrix[pas + j] * matrix[pas + j];
     }
-    x[0] -= sqrt(norm);
+    x[0] -= std::sqrt(norm);
     norm -= matrix[pas + i + 1] * matrix[pas + i + 1] - x[0] * x[0];
     norm /= 2;
 }
